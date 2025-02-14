dc0cb1d5160e44bdaadcf1cfca61850cf82a084a
GNANESWARIG <119475080+GNANESWARIG@users.noreply.github.com>
2025-02-14 12:17:09 +0530

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -16,6 +16,6 @@ $conn = new mysqli($host, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("❌ Connection failed: " . $conn->connect_error);
 } else {
-    echo "✅ Database connected successfully!";
+  //  echo "✅ Database connected successfully!";
 }
 ?>
